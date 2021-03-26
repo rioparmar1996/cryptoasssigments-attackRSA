e709847dfb8c35f4b1bacb79662efac0e99f8c27
rioparmar1996 <63126537+rioparmar1996@users.noreply.github.com>
2021-03-26 10:37:22 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,15 +5,34 @@ e=int(input())
 N=int(input())
 print("Enter a cipher text:", end='')
 c=int(input())
-counter=0
-while counter<N+1:
-    a=counter**e
-    if a%N==c%N:
-        m=counter
-    counter+=1
-print("original message:",m)
+
+factors=[]
+p=[]
+q=[]
+x=[]
+count=0
+
+for i in range(1, N+1):
+    if(N%i==0):
+        factors.append(i)
+
+for i in range(0, len(factors)):
+    for j in range(0, len(factors)):
+        if (factors[i]*factors[j]==N):
+            p.append(factors[i])
+            q.append(factors[j])
+            x.append((p[i],q[i]))
+
+for i in range(0, len(x)):
+    x = (p[i]-1)*(q[i]-1)
+    for j in range(1, x):
+        w = j*e
+        if (w % x==1):
+            s=i
+            d=j
+
+m=(c**d) % N
+print("p =",p[s], "\tq =",q[s], "\td =",d, "\tM =",m)
+
 end=time.time()
 print("execution time:",(end-start),"sec")
-
-
-
